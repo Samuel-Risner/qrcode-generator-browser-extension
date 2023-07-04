b0d4786a1acd78f3dcae38f7c9d4fcb42f4fd45b
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-07-04 12:28:28 +0200

    Renamed and removed old code

--- src/popup/index.ts
+++ src/popup/index.ts
@@ -67,38 +67,38 @@ function combineElements({ app, appContents, topBar, popupButton, popupImage, sa
         appContents.appendChild(licensingText);
 }
 
-function setStyleButtonSelected(el: HTMLButtonElement) {
+function _setStyleButtonSelected(el: HTMLButtonElement) {
     el.className = "py-1 px-2 rounded-full bg-neutral-100 dark:bg-slate-500";
 }
 
-function setStyleButtonUnselected(el: HTMLButtonElement) {
+function _setStyleButtonUnselected(el: HTMLButtonElement) {
     el.className = "py-1 px-2 rounded-full bg-transparent";
 }
 
-function setStyles(aE: AllElements, url: string | undefined) {
-    aE.appContents.className = "bg-gray-100 dark:bg-zinc-800 grid grid-cols-1 gap-2 break-words w-80 text-center p-4";
-        aE.topBar.className = "flex flex-row h-8";
-            aE.popupButton.className = "w-7";
-                aE.popupImage.className = "invert-0 dark:invert active:w-6 m-auto";
-            aE.savedUrlsButton.className = "grow bg-zinc-300 rounded-full mx-4 dark:bg-slate-600 text-black dark:text-neutral-200";
-            aE.darkModeButton.className = "text-2xl";
-        aE.savedUrlsMenu.hidden = true;
-        aE.qrCode.className = "m-auto";
-        aE.urlOptionsContainer.className = "flex flex-row";
-            aE.buttonContainer.className = "p-1 rounded-full bg-neutral-300 w-fit dark:bg-slate-600 text-black dark:text-neutral-200";
-            aE.saveUrlButton.className = "mx-auto text-2xl";
-        aE.input.className = "text-center rounded-full p-2 bg-neutral-200 dark:bg-slate-600 text-black dark:text-neutral-200  disabled:text-neutral-400 dark:disabled:text-gray-500 dark:disabled:bg-slate-700";
-        aE.input.disabled = true;
-        aE.qrCodeCorrectLevelDataList.className = "flex flex-row justify-between text-sm ml-2 mr-1 text-black dark:text-neutral-200";
-        aE.licensingText.className = "text-sky-600 dark:text-sky-700 text-sm underline decoration-dashed";
+function setStyles(url: string | undefined, { app, appContents, topBar, popupButton, popupImage, saveUrlButton, darkModeButton, savedUrlsMenu, qrCode, urlOptionsContainer, buttonContainer, thisPageButton, customPageButton, savedUrlsButton, input, qrCodeCorrectLevel, qrCodeCorrectLevelDataList, licensingText }: AllElements) {
+    appContents.className = "bg-gray-100 dark:bg-zinc-800 grid grid-cols-1 gap-2 break-words w-80 text-center p-4";
+        topBar.className = "flex flex-row h-8";
+            popupButton.className = "w-7";
+                popupImage.className = "invert-0 dark:invert active:w-6 m-auto";
+            savedUrlsButton.className = "grow bg-zinc-300 rounded-full mx-4 dark:bg-slate-600 text-black dark:text-neutral-200";
+            darkModeButton.className = "text-2xl";
+        savedUrlsMenu.hidden = true;
+        qrCode.className = "m-auto";
+        urlOptionsContainer.className = "flex flex-row";
+            buttonContainer.className = "p-1 rounded-full bg-neutral-300 w-fit dark:bg-slate-600 text-black dark:text-neutral-200";
+            saveUrlButton.className = "mx-auto text-2xl";
+        input.className = "text-center rounded-full p-2 bg-neutral-200 dark:bg-slate-600 text-black dark:text-neutral-200  disabled:text-neutral-400 dark:disabled:text-gray-500 dark:disabled:bg-slate-700";
+        input.disabled = true;
+        qrCodeCorrectLevelDataList.className = "flex flex-row justify-between text-sm ml-2 mr-1 text-black dark:text-neutral-200";
+        licensingText.className = "text-sky-600 dark:text-sky-700 text-sm underline decoration-dashed";
 
     if (url === undefined) {
-        setStyleButtonSelected(aE.customPageButton);
-        setStyleButtonUnselected(aE.thisPageButton);
-        aE.thisPageButton.disabled = true;
+        _setStyleButtonSelected(customPageButton);
+        _setStyleButtonUnselected(thisPageButton);
+        thisPageButton.disabled = true;
     } else {
-        setStyleButtonSelected(aE.thisPageButton);
-        setStyleButtonUnselected(aE.customPageButton);
+        _setStyleButtonSelected(thisPageButton);
+        _setStyleButtonUnselected(customPageButton);
     }
 }
 
@@ -156,19 +156,6 @@ function setText(aE: AllElements, url: string | undefined) {
     aE.licensingText.title = "This Repo is licensed under the MIT license. This does not include the file 'qrcode.js' which is located in 'extension/js/'. The file is licensed under the MIT license by davidshimjs. For more information see this extensions GitHub repo: https://github.com/Samuel-Risner/qrcode-generator-browser-extension#readme";
 }
 
-function createQrCode(aE: AllElements, url: string | undefined): any {
-    if (url === undefined) {
-        // @ts-ignore
-        const qrCode = new QRCode(aE.qrCode, { text: "", correctLevel: QRCode.CorrectLevel.H });
-        qrCode.clear();
-
-        return qrCode;
-    }
-
-    // @ts-ignore
-    return new QRCode(aE.qrCode, { text: url, correctLevel: QRCode.CorrectLevel.H });
-}
-
 function setDarkMode(aE: AllElements, isDark: boolean) {
     if (isDark) {
         aE.app.classList.add("dark");
@@ -189,8 +176,8 @@ function addEvents(aE: AllElements, qrCode: any, url: string | undefined, savedU
 
     if (url !== undefined) {
         aE.thisPageButton.onclick = () => {
-            setStyleButtonSelected(aE.thisPageButton);
-            setStyleButtonUnselected(aE.customPageButton);
+            _setStyleButtonSelected(aE.thisPageButton);
+            _setStyleButtonUnselected(aE.customPageButton);
             aE.input.disabled = true;
             aE.input.value = url;
             qrCode.clear();
@@ -198,8 +185,8 @@ function addEvents(aE: AllElements, qrCode: any, url: string | undefined, savedU
         }
 
         aE.customPageButton.onclick = () => {
-            setStyleButtonSelected(aE.customPageButton);
-            setStyleButtonUnselected(aE.thisPageButton);
+            _setStyleButtonSelected(aE.customPageButton);
+            _setStyleButtonUnselected(aE.thisPageButton);
             aE.input.disabled = false;
         }
     }
@@ -242,6 +229,8 @@ function addSavedUrl(url: string, aE: AllElements, qrCode: any) {
 }
 
 async function main() {
+    console.log("foo");
+
     let urlPromise: Promise<browser.tabs.Tab[]> = browser.tabs.query({ currentWindow: true, active: true });
 
     const allElements = createAndGetElements();
@@ -249,12 +238,11 @@ async function main() {
 
     let url: string | undefined = (await urlPromise)[0].url;
 
-    // const code = new Code(url, allElements.qrCode, {});
-
-    setStyles(allElements, url);
-    setText(allElements, url);
-    const qrCode = createQrCode(allElements, url);
+    const qrCode = new Code(url, allElements.qrCode, {});
     const savedUrls = new SavedUrls((url: string) => { addSavedUrl(url, allElements, qrCode); }, allElements.savedUrlsMenu);
+    
+    setStyles(url, allElements);
+    setText(allElements, url);
     addEvents(allElements, qrCode, url, savedUrls);
 
     const msg: GetDarkModeMessage = { type: MessageTypes.GetDarkMode };
